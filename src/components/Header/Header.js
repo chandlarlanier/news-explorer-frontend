9ecfd0f15067de488ffb80f3c28b6a4d7f67d702
Chandlar Lanier <cklanier@outlook.com>
2023-12-23 13:12:07 -0500

    Add initial markup for navigation bar when user is logged in

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ import "./Header.css";
 function Header({openPopup}) {
   return (
     <div className="header">
-      <Navigation openPopup={openPopup}/>
+      <Navigation openPopup={openPopup} isLoggedIn={true}/>
       <div className="header__text">
         <h1 className="header__title">What's going on in the world?</h1>
         <p className="header__description">
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,21 +1,40 @@
 // import { Link } from "react-router-dom";
 import "./Navigation.css";
 import logo from "../../images/logo.svg";
+import signOutIcon from "../../images/sign-out-icon.svg";
 
-function Navigation({openPopup}) {
+function Navigation({ openPopup, isLoggedIn }) {
   return (
     <div className="navigation">
       <div className="navigation__left">
         {/* <Link to="/"> */}
-          <img className='navigation__logo' src={logo} alt="News Explorer logo" />
+        <img className="navigation__logo" src={logo} alt="News Explorer logo" />
         {/* </Link> */}
       </div>
-      <div className="navigation__right">
-        {/* <Link to="/"> */}
-            <div className='navigation__home-link'>Home</div>
-        {/* </Link> */}
-        <button className='navigation__sign-in-button' onClick={() => openPopup('sign-in')}>Sign in</button>
-      </div>
+      {isLoggedIn ? (
+        <div className="navigation__right">
+          {/* <Link to="/"> */}
+          <div className="navigation__home-link">Home</div>
+          {/* </Link> */}
+          <div className="navigation__profile-link">Saved Articles</div>
+          <button className="navigation__sign-out-button">
+            <p>Elise</p>
+            <img src={signOutIcon} alt="Sign out" />
+          </button>
+        </div>
+      ) : (
+        <div className="navigation__right">
+          {/* <Link to="/"> */}
+          <div className="navigation__home-link">Home</div>
+          {/* </Link> */}
+          <button
+            className="navigation__sign-in-button"
+            onClick={() => openPopup("sign-in")}
+          >
+            Sign in
+          </button>
+        </div>
+      )}
     </div>
   );
 }
