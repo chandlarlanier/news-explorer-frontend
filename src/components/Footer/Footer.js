1e843ea4192134f9eed53168b26fdc2aa2507640
Chandlar Lanier <cklanier@outlook.com>
2024-01-06 20:56:15 -0500

    Update footer to use grid layout

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,35 +6,33 @@ import { Link } from "react-router-dom";
 function Footer() {
   return (
     <footer className="footer">
-      <div className="footer__left">
+      <p className="footer__copyright">
         <span>&#169;</span> Supersite, Powered by News API
-      </div>
-      <div className="footer__right">
-        <Link className="footer__home-link" to="/">
-          Home
-        </Link>
-        <a
-          className="footer__tripleten-link"
-          href="https://tripleten.com/"
-          target="_blank"
-        >
-          TripleTen
-        </a>
-        <a
-          className="footer__github-link"
-          href="https://github.com/chandlarlanier"
-          target="_blank"
-        >
-          <img src={githubLogo} alt="GitHub logo" />
-        </a>
-        <a
-          className="footer__linkedin-link"
-          href="https://www.linkedin.com/in/chandlarklanier"
-          target="_blank"
-        >
-          <img src={linkedinLogo} alt="LinkedIn logo" />
-        </a>
-      </div>
+      </p>
+      <Link className="footer__home-link" to="/">
+        Home
+      </Link>
+      <a
+        className="footer__tripleten-link"
+        href="https://tripleten.com/"
+        target="_blank"
+      >
+        TripleTen
+      </a>
+      <a
+        className="footer__github-link"
+        href="https://github.com/chandlarlanier"
+        target="_blank"
+      >
+        <img src={githubLogo} alt="GitHub logo" />
+      </a>
+      <a
+        className="footer__linkedin-link"
+        href="https://www.linkedin.com/in/chandlarklanier"
+        target="_blank"
+      >
+        <img src={linkedinLogo} alt="LinkedIn logo" />
+      </a>
     </footer>
   );
 }
